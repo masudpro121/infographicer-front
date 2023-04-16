7a3a04d798ba9f34db10fa05605f95b0653914b9
masudrana08 <rana028511@gmail.com>
2023-04-16 10:20:45 +0600

    setup done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import { createContext } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
+// Router 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+
+// App Started 
+export const MyContext = createContext()
 function App() {
+  const contextValue = {
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} >
+      <MyContext.Provider value={contextValue} />
+    </RouterProvider>
   );
 }
 
