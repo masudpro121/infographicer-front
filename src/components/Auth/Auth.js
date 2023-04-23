e67c249556bb5e35c9d64fed5e0bcb7887bbdc79
masudrana08 <rana028511@gmail.com>
2023-04-23 06:13:34 +0600

    margin fix

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -56,9 +56,54 @@ function signup() {
   
 function signin() {
     return (
-      <div>Signin</div>
+      <div className='auth'>
+        <div className="card-bg"></div>
+        <div className="card-content">
+            <div className="logo">
+                <img src={Logo} alt="logo" />
+            </div>
+            <div className="card-title mb-3">
+                <h5>Create an account</h5>
+                <small>Already have an account? <span>Log in</span></small> 
+            </div>
+            <div className="myform">
+                <div>
+                    <label htmlFor="email">Email address</label> <br/>
+                    <input id="email" type="email" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label> <br/>
+                    <input id="password" type="password" />
+                </div>
+                <div>
+                    <label htmlFor="residence">Country of residence</label> <br/>
+                    <select name="Residence" id="residence">
+                        <option value="saudi">Saudi Arab</option>
+                        <option value="bangladesh">Bangladesh</option>
+                        <option value="pakistan">Pakistan</option>
+                    </select>
+                </div>
+            </div>
+            <div className="mt-3">
+                <button>Create an account</button>
+            </div>
+            <div className="f-auth mt-3">
+                <div >
+                    <small>Or, continue with</small>
+                </div>
+                <div className='mx-1'>
+                    <img className='google' src={Google} alt="Google" /> <small>Or</small>
+                </div>
+                <div>
+                    <img className='facebook' src={Facebook} alt="Facebook" />
+                </div>
+            </div>
+        </div>
+      </div>
     )
 }
+  
+
 
 export const Signup = withNavbar(signup)
 export const Signin = withNavbar(signin)
