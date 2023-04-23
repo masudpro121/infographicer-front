d16eca36327bc2801d7a1cf0f855512303b41f14
masudrana08 <rana028511@gmail.com>
2023-04-23 16:24:34 +0600

    projects card

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -4,75 +4,22 @@ import './projects.css'
 import Paginate from '../../components/Paginate/Paginate'
 import { useContext } from 'react'
 import { MyContext } from '../../App'
+import Cards from '../../components/Cards/Cards'
 
 function Projects() {
   const {currentProjectPage} = useContext(MyContext)
   const perPageItem = 4
   const fakeprojects = [
-    {
-      name: 'Project 1',
-      content: `The best product start with figma
-      Design with real data
-      lightning fast prototyping
-      fastest way to organize
-      work as the speed of thought.`
-    },
-    {
-      name: 'Project 2',
-      content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium nobis voluptatibus, numquam amet repellat totam iure est. Sequi, illum?`
-    },
-    {
-      name: 'Project 3',
-      content: `The best product start with figma
-      Design with real data
-      lightning fast prototyping
-      fastest way to organize
-      work as the speed of thought.`
-    },
-    {
-      name: 'Project 4',
-      content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium nobis voluptatibus, numquam amet repellat totam iure est. Sequi, illum?`
-    },
-    {
-      name: 'Project 5',
-      content: `The best product start with figma
-      Design with real data
-      lightning fast prototyping
-      fastest way to organize
-      work as the speed of thought.`
-    },
-    {
-      name: 'Project 6',
-      content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium nobis voluptatibus, numquam amet repellat totam iure est. Sequi, illum?`
-    },
-    {
-      name: 'Project 7',
-      content: `The best product start with figma
-      Design with real data
-      lightning fast prototyping
-      fastest way to organize
-      work as the speed of thought.`
-    },
-    {
-      name: 'Project 8',
-      content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium nobis voluptatibus, numquam amet repellat totam iure est. Sequi, illum?`
-    },
-    {
-      name: 'Project 9',
-      content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium nobis voluptatibus, numquam amet repellat totam iure est. Sequi, illum?`
-    },
-    {
-      name: 'Project 10',
-      content: `The best product start with figma
-      Design with real data
-      lightning fast prototyping
-      fastest way to organize
-      work as the speed of thought.`
-    },
-    {
-      name: 'Project 11',
-      content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad laudantium nobis voluptatibus, numquam amet repellat totam iure est. Sequi, illum?`
-    },
+    {name: 'Project 1',prompt: 'This is prompt',value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, assumenda.',tag: 'web'},
+    {name: 'Project 1',prompt: 'This is prompt',value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, assumenda.',tag: 'web'},
+    {name: 'Project 1',prompt: 'This is prompt',value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, assumenda.',tag: 'web'},
+    {name: 'Project 1',prompt: 'This is prompt',value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, assumenda.',tag: 'web'},
+    {name: 'Project 1',prompt: 'This is prompt',value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, assumenda.',tag: 'web'},
+    {name: 'Project 1',prompt: 'This is prompt',value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, assumenda.',tag: 'web'},
+    {name: 'Project 1',prompt: 'This is prompt',value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, assumenda.',tag: 'web'},
+    {name: 'Project 1',prompt: 'This is prompt',value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, assumenda.',tag: 'web'},
+    {name: 'Project 1',prompt: 'This is prompt',value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, assumenda.',tag: 'web'},
+ 
     
   ]
   const myprojects = fakeprojects.slice((currentProjectPage*perPageItem)-perPageItem, currentProjectPage*perPageItem)
@@ -80,23 +27,33 @@ function Projects() {
   return (
     <div className='projects'>
       <div className='top'>
-        <h6>Projects</h6>
+        <h5>Projects</h5>
         <button>Create new project</button>
       </div>
       <div className='search'>
-        <div>
-          <input type="text" placeholder='Search by project name' />
-          <input type="text" placeholder='Search by project tag' />
-          <input type="text" placeholder='Search by project keyword' />
-          <input type="text" placeholder='Search by project username' />
-          <input type="text" placeholder='Search by project date started' />
-          <input type="text" placeholder='Search by project date end' />
-        </div>
+          <div>
+            <input type="text" placeholder='Search by project name' />
+          </div>
+          <div>
+            <input type="text" placeholder='Search by project tag' />
+          </div>
+          <div>
+            <input type="text" placeholder='Search by project keyword' />
+          </div>
+          <div>
+            <input type="text" placeholder='Search by project username' />
+          </div>
+          <div>
+            <input type="text" placeholder='Search by project date started' />
+          </div>
+          <div>
+            <input type="text" placeholder='Search by project date end' />
+          </div>
         <div>
           <button>Search</button>
         </div>
       </div>
-      <div className="p-cards">
+      {/* <div className="p-cards">
         {
           myprojects.map((project,id)=>{
             return (
@@ -110,7 +67,8 @@ function Projects() {
             )
           })
         }
-      </div>
+      </div> */}
+      <Cards data={fakeprojects}/>
       <div>
        {
         Math.ceil(fakeprojects.length/6) >1 && <Paginate totalPage={Math.ceil(fakeprojects.length/perPageItem)} />
